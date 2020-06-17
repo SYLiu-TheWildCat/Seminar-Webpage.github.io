2232dd6a5274f12eb283bb8fef1397ae7306600c
SYLiu-TheWildCat <51726975+SYLiu-TheWildCat@users.noreply.github.com>
2020-06-17 13:04:17 +0800

    20200617

---
+++ mathsetting.js
@@ -0,0 +1,10 @@
+window.MathJax = {
+    tex:{
+        tags: 'ams',
+        macros: {
+            im: "{\\mathrm {im}}",
+            dvol: "{\\mathrm {dvol}}",
+            index: "{\\mathrm {index}}"
+        }
+    }
+};
