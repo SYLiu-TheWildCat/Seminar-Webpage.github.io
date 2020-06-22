876f8e94b145a9a225c5bf46b2aa127f37bc37e1
SYLiu-TheWildCat <51726975+SYLiu-TheWildCat@users.noreply.github.com>
2020-06-22 23:07:00 +0800

    20200622

--- mathsetting.js
+++ mathsetting.js
@@ -1,12 +1,14 @@
 window.MathJax = {
     tex:{
+        extensions: ["autoload-all.js"],
         tags: 'ams',
         macros: {
             im: "{\\mathrm {im}}",
             dvol: "{\\mathrm {dvol}}",
             index: "{\\mathrm {index}}",
             Hom: "{\\mathrm {Hom}}",
-            diff: "{\\mathrm {d}}"
+            diff: "{\\mathrm {d}}",
+            iIm: "{\\mathrm {Im}}",
         }
     }
 };
